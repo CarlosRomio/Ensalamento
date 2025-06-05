e179999c278a934955ba671fa2075fb70c4cc43d
CarlosRomio <130614865+CarlosRomio@users.noreply.github.com>
2025-06-05 17:25:00 -0300

    conexão com banco na nuvem

--- src/main/java/br/edu/atitus/remediario/RemediarioApplication.java
+++ src/main/java/br/edu/atitus/remediario/RemediarioApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "br.edu.atitus.remediario")
+//@ComponentScan(basePackages = "br.edu.atitus.remediario")
 public class RemediarioApplication {
 
 	public static void main(String[] args) {
